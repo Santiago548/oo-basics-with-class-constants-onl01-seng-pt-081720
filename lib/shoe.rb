728519feb7ff81a277a211acd5605f20922120a2
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-11 00:14:49 +0000

    Automatically backed up by Learn

---
+++ lib/shoe.rb
@@ -0,0 +1,15 @@
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
+  end
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
