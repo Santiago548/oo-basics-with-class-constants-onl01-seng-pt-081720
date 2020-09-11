759cff18c665bee70e8571afbabf0df678811da2
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-11 00:16:12 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,8 @@
 class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
-
+    
+    BRANDS = []
+    
   def initialize(brand)
     @brand = brand
     @color = color
@@ -12,4 +14,8 @@ class Shoe
     @condition = "new"
     puts "Your shoe is as good as new!"
   end
+
+  def brand=(brand)
+    @brand = brand
+    BBRANDS << brand
 end
